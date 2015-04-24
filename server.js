281e2771f0e60a97364d7c0a43926982aa2b8875
Vivek <vtumrukota@gmail.com>
2015-04-24 01:00:34 -0700

    Adjust Server Code for Hello World

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var app = express();
 
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/', function(req, res) {
-  res.send('hello world');
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+
 });
